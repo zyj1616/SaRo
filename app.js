f99b553e85ec6b09d0b34dfdd80f6f08b3e6db39
zyj1616 <y1zhou@ucsd.edu>
2019-02-11 00:27:53 -0800

    test

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 
-let express = require("express");
-let app = express();
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose")
+mongoose.connect("mongodb://localhost/msg_app")
 
 app.use(express.static("public"));
 
@@ -13,6 +15,35 @@ app.get("/events", function (req, res) {
     res.render("archives/events.ejs")
 });
 
-app.listen(3000,'localhost', function(){
+app.get("/danmu", function (req, res) {
+    res.render("danmu/danmu.ejs")
+});
+
+const msgSchema = new mongoose.Schema({
+	name: String,
+	time: Date,
+	content: String
+});
+
+const Msg = mongoose.model("Msg",msgSchema);
+
+app.get("/danmu/",function(req, res) {
+	const wanted = req.query.name;
+
+	Msg.find({name:wanted},function(err, msgs){
+		if(err){
+			console.log("error");
+		}
+		else{
+			res.render("danmu/danmu.ejs")
+			console.log(msgs);
+		}
+	});
+});
+
+
+app.listen(3000,'0.0.0.0', function(){
     console.log("started");
 });
+
+
